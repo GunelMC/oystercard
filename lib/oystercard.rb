0f4648dad507dcce5e23470bb698172370454da4
“Jonny <jonny.kelly192@gmail.com>
2021-05-04 17:33:33 +0100

    adds max balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
  class Oystercard
-
+  MAX_BALANCE = 90
   attr_reader :balance
     
   def initialize
@@ -7,7 +7,12 @@
   end
   
   def top_up(money)
+    fail "Exceeds maximum balance of #{MAX_BALANCE}" if max_balance?(money)
     @balance += money
   end
 
+ private
+  def max_balance?(money)
+    @balance + money > MAX_BALANCE
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,5 +14,11 @@ describe Oystercard do
     it 'increments the balance by given amount' do
       expect { subject.top_up(50) }.to change {subject.balance}.by(50)
     end
+
+    it 'raises an error if balance exceeds maximum' do
+
+        subject.top_up(50)
+        expect { subject.top_up(45) }.to raise_error "Exceeds maximum balance of #{described_class::MAX_BALANCE}"
+    end
   end 
 end
